011289be01dd1858f67813b46fb9f62fd7236f8a
EnzoBtv <enzoabatrov@hotmail.com>
2022-05-24 23:37:47 -0300

    Add lexer

--- src/lexer.rs
+++ src/lexer.rs
@@ -9,7 +9,12 @@ struct Lexer<'a> {
 
 impl<'a> Lexer<'a> {
     fn new(input: &'a String) -> Lexer<'a> {
-        let mut lexer = Lexer { input: input, position: 0, read_position: 0, ch: '\0' };
+        let mut lexer = Lexer {
+            input: input,
+            position: 0,
+            read_position: 0,
+            ch: '\0',
+        };
         lexer.read_char();
         lexer
     }
@@ -20,7 +25,7 @@ impl<'a> Lexer<'a> {
         } else {
             self.ch = match self.input.chars().nth(self.read_position) {
                 Some(ch) => ch,
-                None => '\0'
+                None => '\0',
             }
         }
 
@@ -28,71 +33,109 @@ impl<'a> Lexer<'a> {
         self.read_position = self.read_position + 1;
     }
 
-    fn read(&mut self, f: fn (char) -> bool) -> String {
+    fn read(&mut self, f: fn(char) -> bool) -> String {
         let position = self.position;
         while f(self.ch) {
             self.read_char();
         }
 
         return String::from(&self.input[position..self.position]);
-
     }
 
     fn skip_whitespace(&mut self) {
         loop {
             match self.ch {
                 ' ' | '\t' | '\n' | '\r' => self.read_char(),
-                _ => break
+                _ => break,
             }
         }
     }
-    
+
+    fn peek_char(&mut self) -> char {
+        if self.read_position >= self.input.len() {
+            return '\0';
+        }
+
+        return match self.input.chars().nth(self.read_position) {
+            Some(ch) => ch,
+            None => '\0',
+        };
+    }
+
     fn next_token(&mut self) -> Token {
         self.skip_whitespace();
         println!("ATTENTION HERE");
         println!("VALUE: {}", self.ch);
         let token = match self.ch {
-            '=' => Token::ASSIGN,
-            ';' => Token::SEMICOLON, 
+            '=' => {
+                let peek_char = self.peek_char();
+                self.read_char();
+                if peek_char == '=' {
+                    self.read_char();
+                    return Token::EQ;
+                }
+                return Token::ASSIGN;
+            }
+            ';' => Token::SEMICOLON,
             '(' => Token::LPAREN,
             ')' => Token::RPAREN,
             ',' => Token::COMMA,
             '+' => Token::PLUS,
             '{' => Token::LBRACE,
             '}' => Token::RBRACE,
+            '-' => Token::MINUS,
+            '!' => {
+                let peek_char = self.peek_char();
+                self.read_char();
+                if peek_char == '=' {
+                    self.read_char();
+                    return Token::NOTEQ;
+                }
+                return Token::BANG;
+            }
+            '*' => Token::ASTERISK,
+            '/' => Token::SLASH,
+            '<' => Token::LT,
+            '>' => Token::GT,
             '\0' => Token::EOF,
             'A'..='Z' | 'a'..='z' => {
                 let identifier = self.read(Util::is_letter);
-                return Token::lookup_ident(identifier)
-            },
-            '0'..='9' => {
-                return Token::INT(self.read(Util::is_number))
+                return Token::lookup_ident(identifier);
             }
+            '0'..='9' => return Token::INT(self.read(Util::is_number)),
             _ => Token::ILLEGAL,
         };
 
         self.read_char();
-        return token
+        return token;
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use std::vec;
-    use crate::token::Token::{*, self};
     use super::Lexer;
+    use crate::token::Token::{self, *};
+    use std::vec;
 
     fn test_tokens(expected_result: Vec<Token>, mut lexer: Lexer) {
         for expected_token in expected_result {
             let parsed_token = lexer.next_token();
 
-            assert_eq!(parsed_token, expected_token, "testing the equality of token: {:?} and parsedToken: {:?}", expected_token, parsed_token);
-       
+            assert_eq!(
+                parsed_token, expected_token,
+                "testing the equality of token: {:?} and parsedToken: {:?}",
+                expected_token, parsed_token
+            );
+
             match (&parsed_token, &expected_token) {
-                (INT(x), INT(y)) => assert_eq!(x, y, "testing the equality of the content of the token {:?} and parsedToken: {:?}", expected_token, parsed_token),
+                (INT(x), INT(y)) => assert_eq!(
+                    x, y,
+                    "testing the equality of the content of the token {:?} and parsedToken: {:?}",
+                    expected_token, parsed_token
+                ),
                 _ => assert!(true),
             };
-        }      
+        }
     }
 
     #[test]
@@ -103,13 +146,14 @@ mod tests {
         ];
 
         let lexer = Lexer::new(&input);
-    
+
         test_tokens(expected_result, lexer)
     }
 
     #[test]
     fn test_next_token_valid_code() {
-        let input = String::from("
+        let input = String::from(
+            "
             let five = 5;
             let ten = 10;
             let add = fn(x, y) {
@@ -117,12 +161,23 @@ mod tests {
             };
 
             let result = add(five, ten);
-        ");
+            !-/*5;
+            5 < 10 > 5;
+            if (5 < 10) {
+                return true;
+            } else {
+                return false;
+            }
+
+            10 == 10;
+            10 != 9;
+        ",
+        );
         let expected_result = vec![
-            LET, 
-            IDENT(String::from("five")), 
-            ASSIGN, 
-            INT(String::from("5")), 
+            LET,
+            IDENT(String::from("five")),
+            ASSIGN,
+            INT(String::from("5")),
             SEMICOLON,
             LET,
             IDENT(String::from("ten")),
@@ -154,6 +209,43 @@ mod tests {
             COMMA,
             IDENT(String::from("ten")),
             RPAREN,
+            SEMICOLON,
+            BANG,
+            MINUS,
+            SLASH,
+            ASTERISK,
+            INT(String::from("5")),
+            SEMICOLON,
+            INT(String::from("5")),
+            LT,
+            INT(String::from("10")),
+            GT,
+            INT(String::from("5")),
+            SEMICOLON,
+            IF,
+            LPAREN,
+            INT(String::from("5")),
+            LT,
+            INT(String::from("10")),
+            RPAREN,
+            LBRACE,
+            RETURN,
+            TRUE,
+            SEMICOLON,
+            RBRACE,
+            ELSE,
+            LBRACE,
+            RETURN,
+            FALSE,
+            SEMICOLON,
+            RBRACE,
+            INT(String::from("10")),
+            EQ,
+            INT(String::from("10")),
+            SEMICOLON,
+            INT(String::from("10")),
+            NOTEQ,
+            INT(String::from("9")),
             SEMICOLON,
             EOF,
         ];
--- src/token.rs
+++ src/token.rs
@@ -13,7 +13,20 @@ pub enum Token {
     LBRACE,
     RBRACE,
     FUNCTION,
-    LET
+    LET,
+    MINUS,
+    BANG,
+    ASTERISK,
+    SLASH,
+    LT,
+    GT,
+    IF,
+    ELSE,
+    TRUE,
+    FALSE,
+    RETURN,
+    EQ,
+    NOTEQ,
 }
 
 impl Token {
@@ -21,6 +34,11 @@ impl Token {
         match ident.as_str() {
             "fn" => Token::FUNCTION,
             "let" => Token::LET,
+            "if" => Token::IF,
+            "else" => Token::ELSE,
+            "true" => Token::TRUE,
+            "false" => Token::FALSE,
+            "return" => Token::RETURN,
             _ => Token::IDENT(ident),
         }
     }
